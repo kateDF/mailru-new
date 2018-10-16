c4ae4d4c80cfbedcb561108f4ca8c55df42e0ecb
Ekaterina Karpuk <ekaterina_karpuk@epam.com>
2018-10-16 16:41:51 +0300

    Change pageLoadTimeout

--- mailruAT_lab5_6_7/src/test/java/com/epam/mailru/driver/ChromeWebdriver.java
+++ mailruAT_lab5_6_7/src/test/java/com/epam/mailru/driver/ChromeWebdriver.java
@@ -19,7 +19,7 @@ public class ChromeWebdriver {
         if (driver == null) {
             checkDriverProperty(WEBDRIVER_CHROME_DRIVER);
             driver = new ChromeDriver();
-            driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+            driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
             driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
             driver.manage().window().maximize();
         }
